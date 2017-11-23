54867c851b5d3e23ec3d1dde68cba3293806d5f5
Stav Davis <stav.davis@gmail.com>
2017-11-23 14:44:23 -0800

    added POST functionality, per Thinkful exercise

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/shopping-list', (req, res) => {
   res.json(ShoppingList.get());
 });
 
-app.post('/shopping-list', jsonParser, (req, res) => {
+app.post('/shopping-list', jsonParser, (req, res) => { //jsonParser = bodyParser.json - middleware parsing of request's json
   // ensure `name` and `budget` are in request body
   const requiredFields = ['name', 'budget'];
   for (let i=0; i<requiredFields.length; i++) {
@@ -47,10 +47,27 @@ app.post('/shopping-list', jsonParser, (req, res) => {
   res.status(201).json(item);
 });
 
-
 app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
+
+//////////////ADDING CODE FOR POST REQUESTS
+app.post('/recipes', jsonParser, (req, res) => { //jsonParser = bodyParser.json - middleware parsing of request's json
+  // ensure `name` and `ingredients` are in request body
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
